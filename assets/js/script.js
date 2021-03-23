1f739bed8ae7bf4ebfd710b37d5165df7f1ca3c5
Your Name <you@example.com>
2021-03-23 17:25:32 -0400

    prevent default refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 // Pick the pretty button that you wanna do something to; put it in a  var or const
-var buttonEl = document.querySelector("#save-task");
+// (deleted)var buttonEl = document.querySelector("#save-task");
 // buttonEl represents the specific btn with the id*save-task
+
+var formEl = document.querySelector("#task-form");
+// select the entire form area instead of the button
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
 // XXXX.addEventListener("click", function() {...}) <=Actually putting in the function that you want to button to do 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     // whenever this listItemEl/ entire function is trigger, it creates another <li>
     listItemEl.className = "task-item";
@@ -14,7 +19,9 @@ var createTaskHandler = function() {
     // put text.content in the empty <li>
     tasksToDoEl.appendChild(listItemEl);
     //attached the listItemEl into the tasksToDo location where we selected with doc.query.selector
-}
-buttonEl.addEventListener("click", createTaskHandler);
-// when the save task button is clicked, the eventlistenser responses by triggering the createTaskHandler function
+};
+// (deleted)buttonEl.addEventListener("click", createTaskHandler);
+// when the save task button is clicked, the addEventListener responses by triggering the createTaskHandler function
 
+formEl.addEventListener("submit", createTaskHandler);
+// >>Now the script.js file finds the <form> element in the page and saves it to the variable formEl, so that we can interact with the form and access some of its child HTML elements.
